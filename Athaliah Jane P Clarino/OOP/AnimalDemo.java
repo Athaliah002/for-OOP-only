a91a77d7417e08d8c7a1f6de5df7f4bd7e64ee53
Athaliah002 <athaliahjaneclarino@gmail.com>
2024-09-12 12:02:24 +0800

    Add files via upload

---
+++ Athaliah Jane P Clarino/OOP/AnimalDemo.java
@@ -0,0 +1,20 @@
+public class AnimalDemo {
+public static void main(String [] args){
+
+    Animal animal1 = new animal();
+    
+   animal.setName();
+   animal.setId();
+   animal.setAge();
+
+   animal.setName("Mizu");
+   animal.setId(4600);
+   animal.setName(2);
+
+   System.out.printlns(animal1 getsName.());
+   System.out.printlns(animal1 getsId.());
+   System.out.printlns(animal1 getsAge.());
+
+}
+
+}
